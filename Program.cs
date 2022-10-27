5f35ea9636a65c4d1d77f616f9f52c4efaf73be6
DiogoZP <diogozago04@gmail.com>
2022-10-27 14:53:11 -0300

    Abstraindo um celular com POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone(numero: "123", modelo: "Iphone 7", imei: "321", memoria: 32);
+Console.WriteLine($"Modelo:{iphone.Modelo}, Numero:{iphone.Numero}, Memoria:{iphone.Memoria}GB, Imei:{iphone.IMEI}");
+iphone.InstalarAplicativo("Whatsapp");
+iphone.Ligar();
+
+Nokia nokia = new Nokia(numero: "54321", modelo: "Nokia C20", imei: "12345", memoria: 32);
+Console.WriteLine($"Modelo:{nokia.Modelo}, Numero:{nokia.Numero}, Memoria:{nokia.Memoria}GB, Imei:{nokia.IMEI}");
+nokia.InstalarAplicativo("Telegram");
+nokia.ReceberLigacao();
